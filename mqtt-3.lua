8db1f7c38472c6d8c87ca9ec6b8021682919f3cc
bengal2009 <bengal168>
2015-07-26 06:30:06 +0800

    'home'

--- mqtt-3.lua
+++ mqtt-3.lua
@@ -8,11 +8,16 @@ function dispatch(m,t,pl)
 end
 function topic1func(m,pl)
  print("get1: "..pl)
-pl1='{"led":"1"}'
-local data = cjson.decode(pl1)
+local data = cjson.decode(pl)
 print(pl)
-print(pl1)
 print(data.led) 
+if(data.led=="1") then 
+gpio.mode(pin,gpio.OUTPUT)
+gpio.write(pin,gpio.HIGH)
+print ("LED1")
+else
+gpio.write(pin,gpio.LOW)
+end
 end
 m_dis["topic1"]=topic1func
 
@@ -20,12 +25,15 @@ m=mqtt.Client()
 m:on("connect",function(m) 
  print("connection "..node.heap()) 
  m:subscribe("topic1",0,function(m) print("sub done") end)
-
+ --m:publish("topic1","hello",0,0)
  end )
 m:on("offline", function(conn)
     print("disconnect to broker...")
-   end)
+    --print(node.heap())
+end)
 m:on("message",dispatch )
 -- Lua: mqtt:connect( host, port, secure, auto_reconnect, function(client) )
 m:connect("192.168.0.1",1883,0,1)
-
+--tmr.alarm(0,10000,1,function() local pl = "time: "..tmr.time() 
+ --m:publish("topic1",pl,0,0)
+ --end)
